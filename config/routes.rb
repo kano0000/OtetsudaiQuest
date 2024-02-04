64cd893a157ad6ba188e6a7ff468c4c38064f81a
ChisatoKano <129806087+kano0000@users.noreply.github.com>
2024-02-04 04:54:15 +0000

    create:task_controller

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   resources :children, only: [:new, :create, :show, :edit, :update]
   resources :rewards, only: [:new, :create, :index, :show, :edit, :update]
   resources :task_lists, only: [:index, :create, :edit, :update, :destroy]
+  resources :tasks, only: [:index, :new, :create, :edit, :update]
 end
