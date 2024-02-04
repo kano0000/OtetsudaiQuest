0cc46c3b850467fdb05081d58cedc789cd860f24
ChisatoKano <129806087+kano0000@users.noreply.github.com>
2024-02-04 09:52:22 +0000

    add:task_status

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,7 +6,7 @@ class Task < ApplicationRecord
   has_one_attached :quest_image
   
   # 制作ステータス => 0:準備中 1:実行中 2:完了報告 3:やりなおし 4:完了
-  enum making_status: {
+  enum status: {
     preparing: 0,
     in_progress: 1,
     reported_complete: 2,
