cc4a7eafe386e8a90c20541c25d456bc52d05dd9
ChisatoKano <129806087+kano0000@users.noreply.github.com>
2024-02-04 04:42:56 +0000

    create:enum

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,13 @@
 class Task < ApplicationRecord
+  belongs_to :task_list
+  
+  # 制作ステータス => 0:準備中 1:実行中 2:完了報告 3:やりなおし 4:完了
+  enum making_status: {
+    preparing: 0,
+    in_progress: 1,
+    reported_complete: 2,
+    again: 3,
+    completed: 4
+  }
+
 end
