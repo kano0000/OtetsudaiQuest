a35c07e24105b9095cb7f9aaf51541d4470fc5fd
ChisatoKano <129806087+kano0000@users.noreply.github.com>
2024-02-10 12:25:15 +0000

    update:child_model

--- app/models/child.rb
+++ app/models/child.rb
@@ -30,5 +30,13 @@ class Child < ApplicationRecord
   # def num_clear
   #   child_tasks.count
   # end
-
+  
+  # ステータス => 0:準備中 1:実行中 2:完了報告 3:やりなおし 4:完了
+  enum status: {
+    preparing: 0,
+    in_progress: 1,
+    reported_complete: 2,
+    again: 3,
+    completed: 4
+  }
 end
