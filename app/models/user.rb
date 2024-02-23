b2b6271ea54b566f7cff70c25e9a493e91ba7700
ChisatoKano <129806087+kano0000@users.noreply.github.com>
2024-02-23 03:37:01 +0000

    update:user_validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :children, dependent: :destroy
   has_many :rewards, dependent: :destroy
   has_many :task_lists, dependent: :destroy
@@ -13,27 +13,27 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
   validates :name, presence: true, length: {in: 2..20}
-  
+
   GUEST_USER_EMAIL = "guest@example.com"
-  
+
   def self.guest
     find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
     user.password = SecureRandom.urlsafe_base64
     user.name = "guestuser"
     end
   end
-  
+
   def guest_user?
     email == GUEST_USER_EMAIL
   end
-  
+
   def get_profile_image(width,height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image1.png')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')      
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     profile_image.variant(resize_to_limit: [width,height]).processed
   end
 
-  
+
 end
